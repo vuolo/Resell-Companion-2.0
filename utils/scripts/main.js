25a74eefb5c347209f995a2e1dc1b72f5ccce9fa
michaelvuolo <michaelvuolo1@gmail.com>
2020-06-09 04:55:24 -0400

    fully implemented filtered area and modal

--- utils/scripts/main.js
+++ utils/scripts/main.js
@@ -9,7 +9,7 @@ window.companionSettings = {
   // language: "en",
   language: "es",
   // theme: "light",
-  theme: "dark",
+  theme: "dark"
 };
 
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36';
